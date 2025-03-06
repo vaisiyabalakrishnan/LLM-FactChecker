b4159a8c75c919d5df42a832e28afa2e2ae82bc5
atharshlakshmi <77614793+atharshlakshmi@users.noreply.github.com>
2025-03-06 18:10:59 +0800

    Update factcheck.py

--- backend/factcheck.py
+++ backend/factcheck.py
@@ -2,19 +2,14 @@
 # need to format the response such that it can be displayed clearly on the website
 # check input: the type of info given
 
-import os
-from dotenv import load_dotenv
+
 from huggingface_hub import InferenceClient
 import json
 
-
-
 def fact_check(claim, evidence):
-    load_dotenv()
-
     client = InferenceClient(
 	provider="novita",
-	api_key=os.getenv("LLAMA_KEY"))
+	api_key="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")
 
     evidence = "\n".join([f"Evidence: {title}" for title in evidence])
     
